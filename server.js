f67ee8da9b9cfb3fc642e1d25c780fea0689e829
Stephane Duhamel <Stephane.Duhamel@thalesesecurity.com>
2019-02-19 15:46:34 -0800

    Updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/bluetooth-demo'));
 app.use(cors()); //<-- That`s it, no more code needed!
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/bluetooth-demo/index.html'));
