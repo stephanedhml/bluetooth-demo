972ee47f3e2627f70e5d1f458a4c68eff35d5de4
Stephane Duhamel <Stephane.Duhamel@thalesesecurity.com>
2019-02-19 14:47:49 -0800

    Added cors to express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 //Install express server
+const cors = require('cors'); //<-- required installing 'cors' (npm i cors --save)
 const express = require('express');
 const path = require('path');
 
@@ -7,6 +8,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/bluetooth-demo'));
+app.use(cors()); //<-- That`s it, no more code needed!
 
 app.get('/*', function(req,res) {
 
