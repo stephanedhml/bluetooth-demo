b885da472d7b60edd318ba583296aaa286463ad1
Stephane Duhamel <Stephane.Duhamel@thalesesecurity.com>
2019-02-20 12:56:52 -0800

    added cors to express.js

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const app = express();
 
 app.options('*', cors())
 
-// var whitelist = ['https://bluetooth-demo-app.herokuapp.com', 'https://ec2-35-172-115-4.compute-1.amazonaws.com']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-//
+var whitelist = ['https://bluetooth-demo-app.herokuapp.com', 'https://ec2-35-172-115-4.compute-1.amazonaws.com/vts/rest/v2.0/detokenize', 'https://ec2-35-172-115-4.compute-1.amazonaws.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 // app.get('/*', cors(corsOptions), function (req, res, next) {
 //   res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
 // })
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/bluetooth-demo'));
-app.get('/*', function(req,res) {
+app.get('/*', cors(corsOptions), function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/bluetooth-demo/index.html'));
 });
 
